dd46926a9218f4c1b1946ff269250cbf7b45adc9
ali <ali0995559052@gmail.com>
2020-12-21 05:50:34 -0800

    add server worker

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,7 +2,7 @@
 if('serviceWorker' in navigator){
   // register services
   window.addEventListener('load',()=>{
-      navigator.serviceWorker.register('../sw_caches.js').then(reg=>console.log('service worker register')).catch(err=>console.log(`service worker:error:${err}`))
+      navigator.serviceWorker.register('./sw_caches.js').then(reg=>console.log('service worker register')).catch(err=>console.log(`service worker:error:${err}`))
   })
   
 }
